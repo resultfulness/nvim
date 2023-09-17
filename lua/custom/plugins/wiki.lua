10b2ea721b1bafe1acf6a39787501c6a3fd118b0
resultfulness <alicja.rrosiak@gmail.com>
2023-09-17 16:26:25 +0200

    switch to kiwi.nvim

--- lua/custom/plugins/wiki.lua
+++ lua/custom/plugins/wiki.lua
@@ -1,14 +1,19 @@
 return {
-  'vimwiki/vimwiki',
-  init = function ()
-    vim.g.vimwiki_global_ext = 0
-    vim.g.vimwiki_root = vim.fn.expand('~/personal/notes')
-    vim.g.vimwiki_list = {
-      {
-        path = "~/personal/notes",
-        syntax = "markdown",
-        ext = ".md",
-      },
-    }
-  end
+	'serenevoid/kiwi.nvim',
+	dependencies = {
+		"nvim-lua/plenary.nvim"
+	},
+	opts = {
+		{
+			name = "notes",
+			path = vim.fn.expand('~/personal/notes')
+		},
+	},
+	keys = {
+		{ "<leader>ww", ":lua require(\"kiwi\").open_wiki_index()<cr>", desc = "Open Wiki index" },
+		{ "<leader>wd", ":lua require(\"kiwi\").open_diary_index()<cr>", desc = "Open Diary index" },
+		{ "<leader>wn", ":lua require(\"kiwi\").open_diary_new()<cr>", desc = "Open today's Diary" },
+		{ "<leader>t", ":lua require(\"kiwi\").todo.toggle()<cr>", desc = "Toggle Markdown Task" }
+	},
+	lazy = true
 }
