fcad27840716aa0f90f26039b72696269e4782f7
resultfulness <alicja.rrosiak@gmail.com>
2023-11-29 08:36:29 +0100

    29-11-2023_08-36

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
     local ui = require("harpoon.ui")
 
     vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[A]dd file to harpoon" })
-    vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { desc = "[H]arpoon menu" })
+    vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu, { desc = "[H]arpoon menu" })
 
     vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
     vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
