f4313e4c2253d9372cd668965b960316c8adf0f1
resultfulness <alicja.rrosiak@gmail.com>
2025-01-01 14:41:32 +0100

    experimental colorscheme change

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,7 +1,28 @@
 return {
-    'neanias/everforest-nvim',
+    'catppuccin/nvim',
     version = false,
     lazy = false,
     priority = 1000,
-    config = function () vim.cmd.colorscheme('everforest') end
+    config = function ()
+        require('catppuccin').setup({
+            color_overrides = {
+                mocha = {
+                    text = "#f4cde9",
+                    subtext1 = "#debad4",
+                    subtext0 = "#c8a6be",
+                    overlay2 = "#b293a8",
+                    overlay1 = "#9c7f92",
+                    overlay0 = "#866c7d",
+                    surface2 = "#705873",
+                    surface1 = "#5a455d",
+                    surface0 = "#443145",
+
+                    base = "#352939",
+                    mantle = "#211924",
+                    crust = "#1a1016",
+                }
+            }
+        })
+        vim.cmd.colorscheme('catppuccin')
+    end
 }
