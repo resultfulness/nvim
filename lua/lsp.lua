f1ecb9431e091e0b84593a4543aee36571277ca4
milkoverflow <alicja.rrosiak@gmail.com>
2025-07-25 09:01:28 +0200

    update lsp configs and colors

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,6 +2,7 @@ local servers = {
     lua = {
         cmd = { 'lua-language-server' },
         filetypes = { 'lua' },
+        root_markers = { '.git', '.stylua.toml' },
         settings = {
             Lua = {
                 diagnostics = {
@@ -11,39 +12,38 @@ local servers = {
                     library = vim.api.nvim_get_runtime_file('', true),
                 },
                 telemetry = { enable = false },
-            }
-        }
+            },
+        },
     },
     typescript = {
         cmd = { 'typescript-language-server', '--stdio' },
         filetypes = { 'typescript', 'javascript' },
-        root_markers = { 'package.json' }
+        root_markers = { 'package.json', '.git' },
     },
     rust = {
         cmd = { 'rust-analyzer' },
         filetypes = { 'rust' },
-        root_markers = { 'Cargo.toml' }
+        root_markers = { 'Cargo.toml', '.git' },
     },
     qmlls = {
         cmd = { '/usr/lib/qt6/bin/qmlls', '-E' },
-        filetypes = { 'qml' }
+        filetypes = { 'qml' },
+        root_markers = { '.git' },
     },
-    -- html = {
-    --     cmd = { 'vscode-html-language-server', '--stdio' },
-    --     filetypes = { 'html' },
-    --     init_options = {
-    --         provideFormatter = true,
-    --         embeddedLanguages = { css = true, javascript = true },
-    --         configurationSection = { 'html', 'css', 'javascript' },
-    --     },
-    -- },
     css = {
         cmd = { 'vscode-css-language-server', '--stdio' },
         filetypes = { 'css' },
+        root_markers = { 'package.json', '.git' },
     },
     emmet = {
         cmd = { '/home/alice/.local/share/pnpm/emmet-language-server', '--stdio' },
         filetypes = { 'html', 'css' },
+        root_markers = { 'package.json', '.git' },
+    },
+    svelte = {
+        cmd = { 'svelteserver', '--stdio' },
+        filetypes = { 'svelte' },
+        root_markers = { 'package.json', '.git' },
     }
 }
 
@@ -58,28 +58,32 @@ end
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-    callback = function (event)
+    callback = function(event)
         vim.keymap.set('n', '<f2>', vim.lsp.buf.rename, { buffer = event.buf, desc = 'rename' })
         vim.keymap.set('n', '<leader>.', vim.lsp.buf.code_action, { buffer = event.buf, desc = 'code action' })
-        vim.keymap.set('n', 'grr', require('telescope.builtin').lsp_references, { buffer = event.buf, desc = 'show references' })
-        vim.keymap.set('n', 'grt', require('telescope.builtin').lsp_type_definitions, { buffer = event.buf, desc = 'goto type' })
+        vim.keymap.set('n', 'grr', require('telescope.builtin').lsp_references,
+            { buffer = event.buf, desc = 'show references' })
+        vim.keymap.set('n', 'grt', require('telescope.builtin').lsp_type_definitions,
+            { buffer = event.buf, desc = 'goto type' })
         vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, { buffer = event.buf })
-        vim.keymap.set('n', 'K', function () vim.lsp.buf.hover({
-            border = "rounded"
-        }) end, { buffer = event.buf })
+        vim.keymap.set('n', 'K', function()
+            vim.lsp.buf.hover({
+                border = "rounded"
+            })
+        end, { buffer = event.buf })
 
         -- create a command `:Format` local to the LSP buffer
         vim.api.nvim_buf_create_user_command(
             event.buf,
             'Format',
-            function () vim.lsp.buf.format() end,
+            function() vim.lsp.buf.format() end,
             { desc = 'format current buffer' }
         )
 
         -- highlight occurrences of word under cursor
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-            local highlight_augroup = vim.api.nvim_create_augroup( 'lsp-highlight', { clear = false })
+            local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                 buffer = event.buf,
                 group = highlight_augroup,
@@ -99,7 +103,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
                     vim.api.nvim_clear_autocmds({ group = 'lsp-highlight', buffer = event2.buf })
                 end,
             })
-
         end
     end
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,18 @@
 return {
     {
-        'neanias/everforest-nvim',
+        'rose-pine/neovim',
+        name = 'rose-pine',
         version = false,
         lazy = false,
         priority = 1000,
         config = function ()
-            vim.cmd.colorscheme('everforest')
+            require('rose-pine').setup({
+                disable_background = true,
+                styles = { italic = false },
+            })
+            vim.cmd.colorscheme('rose-pine-moon')
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
     },
     {
